0c472483728322b0203499acfb7fd2501a7eb6b0
Dhina5 <89687768+Dhina5@users.noreply.github.com>
2021-08-31 10:12:49 +0530

    third commit
    third commit

--- project1/urls.py
+++ project1/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
     path('<slug:slug>/', post_detail, name='post_detail'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
